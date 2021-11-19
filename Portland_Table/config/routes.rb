9e726fa39597e80572cf70d29f3a69b3dd1375a8
cjc473 <chris.cheasty3@gmail.com>
2021-11-19 14:10:31 -0800

    Create user and session controllers

--- Portland_Table/config/routes.rb
+++ Portland_Table/config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   root "static_pages#root"
   
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :show]
+    resource :session, only: [:create, :destroy]
+  end
+
   
 end
 
