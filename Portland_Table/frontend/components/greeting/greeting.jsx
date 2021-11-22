0e8a0f922081e3a06e7edef3534f739bb9a0145c
cjc473 <chris.cheasty3@gmail.com>
2021-11-21 20:30:58 -0800

    Add login and signup form

---
+++ Portland_Table/frontend/components/greeting/greeting.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Greeting = (props) => {
+  const { currentUser, logout } = props
+
+  const personalGreeting = () => ( 
+    <div>
+      <h3>Welcome back {`${currentUser.username}`}!</h3>
+      <button onClick={() => logout()}>Logout</button>
+    </div>
+  )
+
+  const sessionLinks = () => ( 
+    <div>
+      <Link to="/signup">Sign Up</Link>
+      <Link to="/login">Login</Link>
+    </div>
+  )
+
+  return currentUser ? personalGreeting() : sessionLinks();
+}
+
+export default Greeting;
