3b10e44b748b761338f48108f8fa1eb54f262a69
cjc473 <chris.cheasty3@gmail.com>
2021-11-23 14:21:18 -0800

    Setup Modal files

---
+++ frontend/components/modal/modal_container.js
@@ -0,0 +1,15 @@
+import { connect } from "react-redux";
+
+const mSTP = state => {
+  return {
+    modal: state.ui.modal
+  };
+};
+
+const mDTP = dispatch => {
+  return {
+    closeModal: () => dispatch(closeModal())
+  };
+};
+
+export default connect(mSTP, mDTP)(Modal);
